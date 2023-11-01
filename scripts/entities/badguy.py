2648b95accf6c34bd70ea0d031a6fe3aba7ce64c
Travis Henderson <TravisHenderson646@gmail.com>
2023-11-01 15:28:01 -0400

    badguy full move choice weighting

--- scripts/entities/badguy.py
+++ scripts/entities/badguy.py
@@ -1,4 +1,4 @@
-from random import random, choice
+from random import random, choice, choices
 from math import floor
 
 import pygame as pg
@@ -15,7 +15,7 @@ class Badguy(PhysicsEntity):
         self.charge = BadguyCharge(self)
         self.jump = BadguyJump(self)
         self.shoot = BadguyShoot(self)
-        self.speed = 0.8
+        self.speed = 0.2
         self.movement.x = -1
         self.hp = 30
         self.status = 'patrol' # 'patrol' never used
@@ -24,12 +24,13 @@ class Badguy(PhysicsEntity):
         self.ticks_since_started_shoot = 500
         self.true_rect = pg.FRect(0, 0, 15, 25)
         self.gravity = 0.07
+        self.moves = {'shoot': 0, 'charge': 0, 'jump': 0}
         
     def update(self, tilemap, player):
         super().update()
         self.hitboxes[0] = self.hurtboxes[0]
         
-        debugger.debug('flag', self.status)
+        debugger.debug('flag', self.moves)
         
         self.invulnerable = False
         if self.ticks_since_got_hit < 6:
@@ -41,12 +42,12 @@ class Badguy(PhysicsEntity):
                 case 0: #right
                     self.vel.x += .8
                 case 3: #up
-                    self.vel.y = -1      
+                    self.vel.y = -0.5   
         
         if not self.active:
             if (self.collisions['right'] and (self.movement.x == 1)) or (self.collisions['left'] and (self.movement.x == -1)):
                 self.movement.x = -self.movement.x
-            if pg.Vector2(self.hurtboxes[0].x, self.hurtboxes[0].y).distance_to((player.hurtboxes[0].x, player.hurtboxes[0].y)) < self.idle_radius:
+            if pg.Vector2(self.hurtboxes[0].centerx, self.hurtboxes[0].centery).distance_to((player.hurtboxes[0].centerx, player.hurtboxes[0].centery)) < self.idle_radius:
                 self.active = True
                 self.status = 'think'     
         else: # perhaps this should automatically check if previous status is the same as current so each status doesn't have to update
@@ -55,7 +56,7 @@ class Badguy(PhysicsEntity):
                     self.think.active = True
                     self.think.ticks_since_started = 0
                     self.set_animation('idle')
-                self.think.update(player)
+                self.think.update(tilemap, player)
             if self.status == 'charge':
                 if not self.charge.active:
                     self.charge.active = True
@@ -91,7 +92,40 @@ class Badguy(PhysicsEntity):
   # TESTESTESTESTEST   
      #   surf.fill((100,100,100), (self.hurtboxes[0].x - offset[0], self.hurtboxes[0].y - offset[1], 15, 25))
       #  surf.fill((100,0, 0), (self.hitboxes[0].x - offset[0], self.hitboxes[0].y - offset[1], self.hitboxes[0].w, self.hitboxes[0].h))
-                   
+
+class BadguyThink:
+    def __init__(self, badguy):
+        self.badguy = badguy
+        self.active = False
+        self.ticks_since_started = 500
+        self.duration = 120
+        self.range_spacing = 50
+        
+    def update(self, tilemap, player):
+        if self.ticks_since_started == 0:
+            for key in self.badguy.moves.keys():
+                self.badguy.moves[key] += 1
+            self.badguy.speed = random() * 0.4            
+            self.badguy.movement.x = choice([-1, 1])
+        if self.ticks_since_started >= self.duration:
+            self.active = False
+            if self.badguy.flip:
+                if tilemap.check_point((self.badguy.hurtboxes[0].left - 4, self.badguy.hurtboxes[0].bottom - 2), self.badguy.hot_chunk):
+                    self.badguy.moves['charge'] += 2
+            else:
+                if tilemap.check_point((self.badguy.hurtboxes[0].right + 4, self.badguy.hurtboxes[0].bottom - 2), self.badguy.hot_chunk):
+                    self.badguy.moves['charge'] += 2                
+            if pg.Vector2(self.badguy.hurtboxes[0].centerx, self.badguy.hurtboxes[0].centery).distance_to((player.hurtboxes[0].centerx, player.hurtboxes[0].centery)) < self.range_spacing:
+                self.badguy.moves['jump'] += 2
+            else:
+                self.badguy.moves['shoot'] += 2
+            self.badguy.status = choices(list(self.badguy.moves.keys()), weights = list(self.badguy.moves.values()))[0]        
+        if player.hurtboxes[0].centerx < self.badguy.hurtboxes[0].centerx:
+            self.badguy.flip = True
+        else:
+            self.badguy.flip = False
+        self.ticks_since_started += 1
+                           
 class BadguyShoot:
     def __init__(self, badguy):
         self.badguy = badguy
@@ -105,6 +139,7 @@ class BadguyShoot:
         
     def update(self, player):
         if self.ticks_since_started == 0:
+            self.badguy.moves['shoot'] = 0
             self.ticks_since_started = 1
             if player.hurtboxes[0].centerx > self.badguy.hurtboxes[0].centerx:
                 self.badguy.movement.x = 1
@@ -132,6 +167,7 @@ class BadguyJump:
         
     def update(self, player):
         if self.ticks_since_started == 0:
+            self.badguy.moves['jump'] = 0
             self.ticks_since_started = 1
             self.badguy.vel.y = -2.1
             self.badguy.speed = 0.4
@@ -155,26 +191,6 @@ class BadguyJump:
             self.badguy.status = 'think' 
 
 
-class BadguyThink:
-    def __init__(self, badguy):
-        self.badguy = badguy
-        self.active = False
-        self.ticks_since_started = 500
-        self.duration = 120
-        
-    def update(self, player):
-        if self.ticks_since_started == 0:
-            self.badguy.speed = random() * 0.4            
-            self.badguy.movement.x = choice([-1, 1])
-        if self.ticks_since_started >= self.duration:
-            self.active = False
-            self.badguy.status = choice(['shoot', 'charge', 'jump'])        
-        if player.hurtboxes[0].centerx < self.badguy.hurtboxes[0].centerx:
-            self.badguy.flip = True
-        else:
-            self.badguy.flip = False
-        self.ticks_since_started += 1
-        
 class BadguyCharge:
     def __init__(self, badguy):
         self.badguy = badguy
@@ -187,6 +203,7 @@ class BadguyCharge:
         
     def update(self, player):
         if self.ticks_since_started == 0:
+            self.badguy.moves['charge'] = 0
             self.rect.x = self.badguy.hurtboxes[0].x
             self.rect.y = self.badguy.hurtboxes[0].y
             self.badguy.hurtboxes[0] = self.rect
